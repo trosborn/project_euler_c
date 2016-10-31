ce7a26068c00921e3b49f7be4910b836b40daf31
Thomas Osborn <thomas@boo.local>
2016-10-30 22:49:54 -0700

    add problem 1, add gitignore

---
+++ problem1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+    int i = 0;
+    int sum = 0; 
+    
+    while ( i < 1000 ) {
+        if ( i % 3 == 0 || i % 5 == 0) {
+            sum += i; 
+        } 
+        i++;
+    }
+    printf("The sum all ints equal to or less than 100 evenly divisible by 3 and 5 is %d \n", sum);
+}
