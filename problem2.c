42505122b77afa5dbed2e3f8a9c70a7f415e4865
Thomas Osborn <thomas@boo.local>
2016-10-31 00:15:11 -0700

    add problem 2

---
+++ problem2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+    int fib[2] = {1, 2};
+    int rolling_sum = 0;
+    
+    while( fib[1] <= 4000000 ) {
+        if( fib[1] % 2 == 0 ) {
+            rolling_sum += fib[1];
+        }
+        int tmp_sum = fib[0] + fib[1];
+        fib[0] = fib[1];
+        fib[1] = tmp_sum; 
+    }
+    printf("The sum of all even terms less than 4 million in the Fibonacci sequence is: %d \n", rolling_sum);
+}
